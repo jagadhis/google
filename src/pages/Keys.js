aae26efc7621709dbaecfe534330331aeba43b01
jagadhis <jwaran78@gmail.com>
2021-10-13 23:24:52 +0530

    commit

---
+++ src/pages/Keys.js
@@ -0,0 +1,3 @@
+export const API_KEY = "AIzaSyCY3rY7ol7Zz0JZenRx47zL25Amu0cXA7U";
+
+export default API_KEY;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -49,7 +49,6 @@ function Search({ hideButtons = false }) {
           </Button>
         </div>
       )}
-      ;
     </div>
   );
 }
