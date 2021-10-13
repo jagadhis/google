ab47192e8d183a62add0672c4dd751b5c5fcf662
jagadhis <jwaran78@gmail.com>
2021-10-13 12:03:09 +0530

    header done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Lets build a google clone 🚀</h1>
-      <Home />
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>this is a new page 🚀</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <h1>Lets build a google clone 🚀</h1>
+      </Router>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./Home.css";
+import { Link } from "react-router-dom";
+import AppsIcon from "@mui/icons-material/Apps";
 
 function Home() {
   return (
-    <div>
-      <h1> hey guys</h1>
-      <div className="home_header"></div>
+    <div className="home">
+      <div className="home_header">
+        <div className="home_headerleft">
+          <Link to="/about">About</Link>
+          <Link to="/Store">Store</Link>
+        </div>
+        <div className="home_headerright">
+          <Link to="/Gmail">Gmail</Link>
+          <Link to="/Images">Images</Link>
+          <AppsIcon />
+        </div>
+      </div>
       <div className="home_body"></div>
     </div>
   );
