72a37be88017ef8f4cd613ee067d0676adeb7d5e
jagadhis <jwaran78@gmail.com>
2021-10-13 10:47:32 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <h1>Lets build a google clone 🚀</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div>
+      <h1> hey guys</h1>
+      <div className="home_header"></div>
+      <div className="home_body"></div>
+    </div>
+  );
+}
+
+export default Home;
